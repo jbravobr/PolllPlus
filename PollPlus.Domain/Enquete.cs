15f5ff89f82d40429abc4371348576a82fc59b05
patriciaruss <pmeirelesruss@gmail.com>
2015-05-18 21:38:51 -0300

    teste

--- PollPlus.Domain/Enquete.cs
+++ PollPlus.Domain/Enquete.cs
@@ -12,8 +12,7 @@ namespace PollPlus.Domain
         public string PerguntaTitulo { get; protected set; }
         public EnumStatusEnquete Status { get; protected set; }
         public EnumTipoImagem TipoImagem { get; protected set; }
-        public string Imagem { get; protected set; }
-        
+        public string Imagem { get; protected set; }        
         public virtual Usuario UsuarioCriador { get; protected set; }
         public virtual Empresa EmpresaProprietaria { get; protected set; }
     }
