8b13d31c10b3ea2dff14e6325535eee821b353c8
Rodrigo Amaro dos Santos Amaral <jbravo.br@gmail.com>
2015-06-23 23:48:37 -0300

    Modificacoes ninject

--- PollPlus/Controllers/AccountAPIController.cs
+++ PollPlus/Controllers/AccountAPIController.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using Ninject;
 using PollPlus.Domain;
+using PollPlus.Helpers;
+using PollPlus.Repositorio;
 using PollPlus.Service.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -15,12 +17,11 @@ namespace PollPlus.Controllers
     [RoutePrefix("maisapi/usuario")]
     public class AccountAPIController : ApiController
     {
-        readonly IUsuarioServiceWEB service;
+        private UsuarioRepositorio service = new UsuarioRepositorio();
 
-        public AccountAPIController(IUsuarioServiceWEB srv)
-        {
-            this.service = srv;
-        }
+        private EnqueteRepositorio enqueteRepo = new EnqueteRepositorio();
+
+        public AccountAPIController() { }
 
         [HttpGet]
         [Route("autenticar/{email}/{senha}")]
@@ -28,12 +29,16 @@ namespace PollPlus.Controllers
         {
             try
             {
-                var autenticado = await this.service.LogarUsuario(email, senha);
+                var _usuario = (await this.service.RetornarTodosUsuarios()).FirstOrDefault(u => u.Email == email);
 
-                if (autenticado)
+                var senhaDecrypt = Util.DescriptarSenha(_usuario.Senha);
+
+                if (_usuario == null)
+                    return Unauthorized();
+                else if (_usuario != null && senha == senhaDecrypt)
                     return Ok();
-
-                return Unauthorized();
+                else
+                    return Unauthorized();
             }
             catch (Exception ex)
             {
@@ -51,7 +56,7 @@ namespace PollPlus.Controllers
             try
             {
                 var usuarioJson = JsonConvert.DeserializeObject<Usuario>(usuario);
-                var retornoInsertUsuario = await this.service.InserirRetornarUsuario(usuarioJson);
+                var retornoInsertUsuario = await this.service.InserirUsuario(usuarioJson);
 
                 return Ok();
             }
@@ -59,6 +64,87 @@ namespace PollPlus.Controllers
             {
                 return InternalServerError(ex);
             }
+        }
+
+        [HttpGet]
+        [Route("enquete/{id}")]
+        public async Task<IHttpActionResult> GetEnquetesPublicas(int id)
+        {
+            List<Enquete> enquetes = null;
+
+            if (id > 0)
+                enquetes = (await enqueteRepo.RetornarTodos()).Where(e => e.Id > id && e.Tipo == Domain.Enumeradores.EnumTipoEnquete.Publica).ToList();
+            else
+                enquetes = (await enqueteRepo.RetornarTodos()).Where(e => e.Tipo == Domain.Enumeradores.EnumTipoEnquete.Publica).ToList();
+
+            if (enquetes != null)
+            {
+                var enquetesJson = JsonConvert.SerializeObject(enquetes);
+                return Ok(enquetesJson);
+            }
+            else
+                return BadRequest("Não há enquetes disponíveis");
+        }
+
+        [HttpGet]
+        [Route("enquete/{id}")]
+        public async Task<IHttpActionResult> GetEnquetesInteresse(int id)
+        {
+            List<Enquete> enquetes = null;
+
+            if (id > 0)
+                enquetes = (await enqueteRepo.RetornarTodos()).Where(e => e.Id > id && e.Tipo == Domain.Enumeradores.EnumTipoEnquete.Interesse).ToList();
+            else
+                enquetes = (await enqueteRepo.RetornarTodos()).Where(e => e.Tipo == Domain.Enumeradores.EnumTipoEnquete.Interesse).ToList();
+
+            if (enquetes != null)
+            {
+                var enquetesJson = JsonConvert.SerializeObject(enquetes);
+                return Ok(enquetesJson);
+            }
+            else
+                return BadRequest("Não há enquetes disponíveis");
+        }
+
+        [HttpPost]
+        [Route("novaenquete")]
+        public async Task<IHttpActionResult> NovaEnquete([FromBody]string enquete)
+        {
+            if (String.IsNullOrEmpty(enquete))
+                return BadRequest("Enquete inválida");
+
+            try
+            {
+                var enqueteJson = JsonConvert.DeserializeObject<Enquete>(enquete);
+                var retornoInsertEnquete = await this.enqueteRepo.InserirEnquete(enqueteJson);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> SalvaRespostas(PerguntaViewModel Pergunta, List<string> resposta)
+        {
+            if (!ModelState.IsValid || !resposta.Any())
+                return View(Pergunta);
+
+            var respostas = MapeiaListaDeRespostas(resposta, Pergunta.Id);
+
+            foreach (var r in respostas)
+            {
+                await this.serviceResposta.InserirResposta(r);
+            }
+
+            Pergunta.Resposta = AutoMapper.Mapper.Map<ICollection<RespostaViewModel>>(respostas.ToList());
+
+            if (await this.servicePergunta.AtualizarPergunta(AutoMapper.Mapper.Map<Pergunta>(Pergunta)))
+                return RedirectToAction("ListarEnquetes");
+            else
+                return View("NovaEnquete");
         }
     }
 }
--- PollPlus/Startup.cs
+++ PollPlus/Startup.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Web.Http;
 
 [assembly: OwinStartupAttribute(typeof(PollPlus.Startup))]
 namespace PollPlus
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-           //ConfigureAuth(app);
-        }
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    var config = new HttpConfiguration();
+        //    WebApiConfig.Register(config);
+
+        //    app.UseNinjectMiddleware(() => NinjectConfig.CreateKernel.Value);
+        //    app.UseNinjectWebApi(config);
+        //}
     }
 }
