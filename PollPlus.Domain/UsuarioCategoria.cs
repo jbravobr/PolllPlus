202dac08761b9aa68ebd30fd44ba646ea99b2d09
Rodrigo Amaro <jbravo_br@hotmail.com>
2015-07-25 11:58:12 -0300

    Ajuste para envio de enquete para amigo

--- PollPlus.Domain/UsuarioCategoria.cs
+++ PollPlus.Domain/UsuarioCategoria.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,11 @@ namespace PollPlus.Domain
     public class UsuarioCategoria : EntityBase
     {
         public int CategoriaId { get; set; }
+        [JsonIgnore]
         public virtual Categoria Categoria { get; set; }
 
         public int UsuarioId { get; set; }
+        [JsonIgnore]
         public virtual Usuario Usuario { get; set; }
     }
 }
--- PollPlus.Domain/UsuarioPlataforma.cs
+++ PollPlus.Domain/UsuarioPlataforma.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,11 @@ namespace PollPlus.Domain
     public class UsuarioPlataforma : EntityBase
     {
         public int UsuarioId { get; set; }
+        [JsonIgnore]
         public virtual Usuario Usuario { get; set; }
 
         public int PlataformaId { get; set; }
+        [JsonIgnore]
         public virtual Plataforma Plataforma { get; set; }
     }
 }
